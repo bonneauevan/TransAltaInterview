36a8a88ef05d5162b73ed33a5b9325ad079da878
Evan Bonneau <bonneauevan@gmail.com>
2022-03-28 18:15:43 -0700

    Adding react app

---
+++ interviewreactapp/src/setupProxy.js
@@ -0,0 +1,14 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7263/api/weather/getWeatherData',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
